edb406baec13e79b6e2d8092fd5301f4b8290c13
kushalpokharel <kusaalpokharel@gmail.com>
2022-04-14 23:36:59 +0545

    navigation added

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import './App.css';
 import { Routes, Route } from 'react-router-dom';
 import Main from './components/Main';
 import SignUp from './components/SignUp';
+import MiniDrawer from './components/NavBar';
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Main />} />
       <Route path="/signup" element={<SignUp/>}/>
+      <Route path="/navbar" element={<MiniDrawer/>}></Route>
     </Routes>
   );
 }
